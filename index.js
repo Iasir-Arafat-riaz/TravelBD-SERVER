262daaaae3eaf18ea4fbe4d1084ad35b9924f6aa
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-27 23:28:39 +0600

    overall commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const ObjectId = require("mongodb").ObjectId;
 const app = express();
 // const port = 5000;
 const port = process.env.PORT || 5000;
@@ -24,12 +24,48 @@ async function run() {
     await client.connect();
     const database = client.db("TravelBD");
     const blogs = database.collection("TravellerEXP");
+    const reviewCollection=database.collection("reviewCollection")
+    const usersCollection = database.collection('users');
     
 app.get("/travellerExperience", async(req,res)=>{
     const travellerExp = blogs.find({})
   const result = await travellerExp.toArray()
   res.json(result)
 })
+
+// get singleBlog details
+
+app.get("/travellerExperience/:id", async(req,res)=>{
+    //  console.log(req.params.id);
+     const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await blogs.findOne(query);
+        res.json(result);
+   })
+
+app.get('/webreviews', async (req, res) => {
+    res.send(await reviewCollection.find({}).toArray());
+});
+
+
+// make admin
+app.put('/users/admin', async (req, res) => {
+    const user = req.body;
+    const filter = { email: user.email };
+    const updateDoc = { $set: { role: "admin" } };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.json(result);
+});
+
+// get admin status
+app.get('/users/:email', async (req, res) => {
+    const user = await usersCollection.findOne({ email: req.params.email });
+    let Admin = false;
+    if (user?.role === 'admin') {
+        Admin = true;
+    };
+    res.json({ Admin: Admin });
+});
 
     
   } finally {
