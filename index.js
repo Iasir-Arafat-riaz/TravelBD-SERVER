f2a798efb1db0a380ae0eceb3bd852b225308baa
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-27 16:36:49 +0600

    new commit

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+// const port = 5000;
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
 
 app.use(cors());
 app.use(express.json());
 const { MongoClient } = require("mongodb");
 
 const uri =
-  "mongodb+srv://travelBD:KlAcunVMDY1MdJm1@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}KlAcunVMDY1MdJm1@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
