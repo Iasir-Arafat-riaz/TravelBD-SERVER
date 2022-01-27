e5287b3441b4595ff59012387c49dd38cd59123b
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-27 16:16:25 +0600

    new commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://travelBD:KlAcunVMDY1MdJm1@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("TravelBD");
+    const blogs = database.collection("TravellerEXP");
+    // query for movies that have a runtime less than 15 minutes
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("running travel BD SERVER");
+});
+
+app.listen(port, () => {
+  console.log("running server on port", port);
+});
