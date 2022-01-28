3755d529fa57f53a58ebf8e15559f3eed37900ab
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-28 20:00:08 +0600

    admin role added

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-
 app.use(cors());
 app.use(express.json());
 const { MongoClient } = require("mongodb");
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -24,50 +21,69 @@ async function run() {
     await client.connect();
     const database = client.db("TravelBD");
     const blogs = database.collection("TravellerEXP");
-    const reviewCollection=database.collection("reviewCollection")
-    const usersCollection = database.collection('users');
-    
-app.get("/travellerExperience", async(req,res)=>{
-    const travellerExp = blogs.find({})
-  const result = await travellerExp.toArray()
-  res.json(result)
-})
+    const reviewCollection = database.collection("reviewCollection");
+    const users = database.collection("users");
 
-// get singleBlog details
+    app.get("/travellerExperience", async (req, res) => {
+      const travellerExp = blogs.find({});
+      const result = await travellerExp.toArray();
+      res.json(result);
+    });
 
-app.get("/travellerExperience/:id", async(req,res)=>{
-    //  console.log(req.params.id);
-     const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await blogs.findOne(query);
-        res.json(result);
-   })
+    // get singleBlog details
 
-app.get('/webreviews', async (req, res) => {
-    res.send(await reviewCollection.find({}).toArray());
-});
+    app.get("/travellerExperience/:id", async (req, res) => {
+      //  console.log(req.params.id);
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await blogs.findOne(query);
+      res.json(result);
+    });
 
+    app.get("/webreviews", async (req, res) => {
+      res.send(await reviewCollection.find({}).toArray());
+    });
 
-// make admin
-app.put('/users/admin', async (req, res) => {
-    const user = req.body;
-    const filter = { email: user.email };
-    const updateDoc = { $set: { role: "admin" } };
-    const result = await usersCollection.updateOne(filter, updateDoc);
-    res.json(result);
-});
+    //post operation for all user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
 
-// get admin status
-app.get('/users/:email', async (req, res) => {
-    const user = await usersCollection.findOne({ email: req.params.email });
-    let Admin = false;
-    if (user?.role === 'admin') {
-        Admin = true;
-    };
-    res.json({ Admin: Admin });
-});
+      const result = await users.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
 
-    
+    //update operation fro users (Upsert)
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const option = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await users.updateOne(filter, updateDoc, option);
+      res.json(result);
+    });
+
+    //Set Admin Role
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await users.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    // check admin for website access (get operation)
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await users.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
   } finally {
     // await client.close();
   }
